fc985b671f776a7d88365507acd6aa65452fb180
eranshmil <eranshmil@users.noreply.github.com>
2019-09-24 17:28:25 +0300

    change sunday to friday

--- content.js
+++ content.js
@@ -3,45 +3,48 @@ function grayOutWeekends(mutList) {
     mutList
       .map(mutation => mutation.addedNodes[0] || mutation.target)
       .filter(node => node.matches && node.matches("[role='main']"))
-      .map(grayOutDay)
+      .map(grayOutDay);
   }
 }
 
 function grayOutDay(mainCal) {
   var nodes = mainCal.querySelectorAll(
-    "div[role='columnheader'],div[data-datekey]:not([jsaction])")
+    "div[role='columnheader'],div[data-datekey]:not([jsaction])"
+  );
   for (node of nodes) {
-    if (node.getAttribute("role") == "columnheader") {
+    if (node.getAttribute('role') == 'columnheader') {
       // This is a gross hack, but inspecting the page shows that the first
       // child is a <span> containing "Sat/Sun" or "Saturday/Sunday"
-      if (node.children[0].innerHTML[0] == "S") {
-        node.style.backgroundColor = "#eeeeee";
+      const day = node.children[0].innerHTML;
+      if (day == 'Friday' || day == 'Saturday') {
+        node.style.backgroundColor = '#eeeeee';
       }
-      continue
+      continue;
     }
-    var datekey = node.getAttribute("data-datekey");
+    var datekey = node.getAttribute('data-datekey');
     if (!datekey) {
-      console.log("could not read expected attribute 'data-datekey'")
-      continue
+      console.log("could not read expected attribute 'data-datekey'");
+      continue;
     }
-    datekey = parseInt(datekey)
-    var year = datekey>>9
-    var month = (datekey & 511)>>5
-    var day = datekey & 31
+    datekey = parseInt(datekey);
+    var year = datekey >> 9;
+    var month = (datekey & 511) >> 5;
+    var day = datekey & 31;
     var date = new Date(
       1970 + year,
       month - 1, // JS date indexes months from 0 for some reason
-      day);
+      day
+    );
     var dayOfWeek = date.getDay();
-    if (dayOfWeek == 0 || dayOfWeek == 6) {
-      node.style.backgroundColor = "#eeeeee";
+    if (dayOfWeek == 5 || dayOfWeek == 6) {
+      node.style.backgroundColor = '#eeeeee';
     }
   }
 }
 
-mut = new MutationObserver(grayOutWeekends)
+mut = new MutationObserver(grayOutWeekends);
 mut.observe(document.body, {
-  "subtree": true,
-  "childList": true,
-  "attributes": true
-})
+  subtree: true,
+  childList: true,
+  attributes: true
+});
